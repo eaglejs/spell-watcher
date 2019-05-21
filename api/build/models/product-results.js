f7e7bc30c3fcbba928135106c59fe6c463635740
eaglejs <joshuaseagle@gmail.com>
2019-05-20 23:19:09 -0400

    updated git ignore

--- api/build/models/product-results.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ProductResults = /** @class */ (function () {
-    function ProductResults() {
-    }
-    return ProductResults;
-}());
-exports.default = ProductResults;
-//# sourceMappingURL=product-results.js.map
