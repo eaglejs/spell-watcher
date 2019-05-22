b73417fe936f302a069523edf67d949758c89aba
eaglejs <joshuaseagle@gmail.com>
2019-05-21 21:59:05 -0400

    added server email.

--- api/app.ts
+++ api/app.ts
@@ -3,13 +3,14 @@ import express from 'express';
 import axios from 'axios';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-
+import nodemailer from 'nodemailer';
 
 import Product from './models/product';
 import ProductResults from './models/product-results';
 import { reject } from 'bluebird';
 
 const parse = require('node-html-parser');
+const config = require('./config.json');
 
 function getPagesDetails(product: Product): Promise<any> {
   let productResults: ProductResults = {
@@ -34,7 +35,7 @@ function getPagesDetails(product: Product): Promise<any> {
         productResults.imgUrl = imgUrl;
         productResults.url = product.url;
 
-        for (let i = 0; i < sizes.length ; i++ ) {
+        for (let i = 0; i < sizes.length; i++) {
           if (sizes[i].text === product.size) {
             productResults.size = product.size;
             productResults.available = sizes[i].attributes.class === 'not-available' ? false : true;
@@ -56,6 +57,28 @@ function getPagesDetails(product: Product): Promise<any> {
   return promise;
 }
 
+function sendEmailNotification(product: Product, productResults: ProductResults): void {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: config
+  });
+
+  const mailOptions = {
+    from: 'spell.watcher.app@gmail.com',
+    to: product.email,
+    subject: `${productResults.title} (${product.size}) NOW AVAILABLE!`,
+    text: `${productResults.title} in the size of (${product.size}) is now Available for ${productResults.price}`
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+}
+
 // Create a new express application instance
 const app: express.Application = express();
 
@@ -64,6 +87,9 @@ app.use(bodyParser.json()); // support json encoded bodies
 
 app.post('/get-product', function (req, res) {
   getPagesDetails(req.body).then((productResults: ProductResults) => {
+    if (productResults.available) {
+      sendEmailNotification(req.body, productResults);
+    }
     res.send(productResults);
   }).catch(error => {
     res.status(500).send(error);
