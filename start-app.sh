f48e2a74d19c5d9dac92203e05a7bbba4c105163
eaglejs <joshuaseagle@gmail.com>
2019-05-24 19:54:07 -0400

    made executables, and modified dockerfiles.

---
+++ start-app.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cd ~/repos/spell-watcher && docker-compose -f docker-compose.production.yml up
+
+sleep 10
---
+++ stop-app.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cd ~/repos/spell-watcher && docker-compose -f docker-compose.production.yml down
+
+sleep 10
